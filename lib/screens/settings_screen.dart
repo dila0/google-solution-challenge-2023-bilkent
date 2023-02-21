6bde498a2be595b01d29dbbbc8814fdfa42c87f8
dila0 <61583782+dila0@users.noreply.github.com>
2023-02-21 09:13:15 +0300

    Screen Files

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -3,21 +3,23 @@ import 'package:google_solution/utilities/constants.dart';
 import 'package:google_solution/utilities/circles.dart';
 import 'package:google_solution/utilities/bottom_bar.dart';
 
-class ProfileScreen extends StatefulWidget{
-  static const String id = 'Profile Screen';
+
+class SettingsScreen extends StatefulWidget{
+  static const String id = 'Settings Screen';
   @override
-  _ProfileScreenState createState() => _ProfileScreenState();
+  _SettingsScreenState createState() => _SettingsScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen>{
+class _SettingsScreenState extends State<SettingsScreen>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: Column(
-        children: const <Widget>[
-          Circles(),
-          Text('profile screen'),
+        children: <Widget>[
+          //top left corner circles
+           Circles(),
+          Text('settings screen'),
         ],
       ),
       bottomNavigationBar: const BottomAppBar(
@@ -27,5 +29,3 @@ class _ProfileScreenState extends State<ProfileScreen>{
     );
   }
 }
-
-
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_solution/utilities/constants.dart';
 import 'package:google_solution/utilities/circles.dart';
+import 'package:google_solution/utilities/register_text_field.dart';
+import 'package:google_solution/utilities/register_button.dart';
+import 'package:google_solution/screens/contact_options_screen.dart';
+import 'package:google_solution/utilities/bottom_text_sign_in.dart';
+import 'package:google_solution/screens/sign_up_screen.dart';
+
+String startText = 'WELCOME BACK!';
+String logInText = 'Log In';
 
 class SignInScreen extends StatefulWidget{
   static const String id = 'Sign In Screen';
@@ -13,10 +21,49 @@ class _SignInScreenState extends State<SignInScreen>{
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Column(
+      body: Stack(
         children: <Widget>[
-          //top left corner circles
-          Circles(),
+          const Circles(),
+          //text fields
+          Center(
+            child: Column(
+              children: [
+                const SizedBox(height: kSignUpScreenDistanceFromTop),
+
+                //started text
+                Text(startText, style: kSignUpInScreen),
+                const SizedBox(height: 25.0),
+
+                //image
+                Image.asset(
+                  'images/audio.png',
+                  height: 300.0,
+                ),
+
+                const SizedBox(height: 15.0),
+                //email
+                const RegisterTextField(hintText: 'Enter Your Email'),
+                const SizedBox(height: kSignUpScreenDistanceBetweenTextFields),
+
+                //password
+                const RegisterTextField(hintText: 'Enter Your Password'),
+                const SizedBox(height: 20.0),
+
+                //register button
+                RegisterButton(title: logInText, routeName: ContactOptionsScreen.id, minWidth: 200.0, height: 40.0),
+                const SizedBox(height: 20.0),
+
+                //bottom text
+                const Center(
+                  child: BottomText(
+                    bottomText: 'Don\'t have an account?  ',
+                    bottomHyperLink: 'Sign Up',
+                    routeName: SignUpScreen.id,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
