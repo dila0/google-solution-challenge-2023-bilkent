7238213fbbbd1e868b9a03c4e1780d6f7acc49ea
Ali Tunahan Işık <tunahan0735@gmail.com>
2023-03-10 19:43:58 +0300

    fixed merge issues

--- lib/screens/contact_options_screen.dart
+++ lib/screens/contact_options_screen.dart
@@ -26,6 +26,7 @@ String welcomeString = 'Welcome';
 List<String> contacts = FirebaseUtility.contacts;
 String message = FirebaseUtility.customMessage;
 bool isDone = false;
+
 class ContactOptionsScreen extends StatefulWidget {
   static const String id = 'Contact Options Screen';
 
@@ -44,11 +45,12 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
   }
 
   Timer? _timer;
-  void _stopTimer(){
-    if(_timer == null) return;
+  void _stopTimer() {
+    if (_timer == null) return;
 
     _timer?.cancel();
   }
+
   void _startTimer() {
     _counter = 5;
 
@@ -57,15 +59,13 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
     }
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       //setState(() {
-      if(_counter > 0) {
+      if (_counter > 0) {
         _counter--;
-      }
-      else
-      {
+      } else {
         _events?.close();
         _events = new StreamController<int>();
         _events?.add(5);
-        Navigator.pop(context,'OK');
+        Navigator.pop(context, 'OK');
         sendSMS(message: message, recipients: contacts);
         launchUrl(Uri.parse("tel://112"));
         setState(() {
@@ -78,9 +78,10 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
       _events?.add(_counter);
     });
   }
+
   void alertD(BuildContext ctx) {
     var alert = AlertDialog(
-        shape: RoundedRectangleBorder(
+        shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(10.0))),
         backgroundColor: Colors.grey[100],
         elevation: 0.0,
@@ -103,8 +104,13 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
                               fontWeight: FontWeight.bold,
                               fontSize: 16),
                         )),
-                    Text('MESSAGE WILL BE SENT TO YOUR CONTACT AND 112 WILL BE CALLED',style:kWarningTextStyle ,textAlign: TextAlign.center,),
-                    Text('${(snapshot.data == null || snapshot.data.toString() == '0') ? '5': snapshot?.data.toString()}'),
+                    Text(
+                      'MESSAGE WILL BE SENT TO YOUR CONTACT AND 112 WILL BE CALLED',
+                      style: kWarningTextStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                        '${(snapshot.data == null || snapshot.data.toString() == '0') ? '5' : snapshot?.data.toString()}'),
                     SizedBox(
                       height: 15,
                     ),
@@ -128,16 +134,15 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
                                 decoration: BoxDecoration(
                                   color: kButtonColor,
                                   borderRadius: BorderRadius.circular(25),
-
                                 ),
                                 child: Center(
                                     child: Text(
-                                      'CANCEL',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    )),
+                                  'CANCEL',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                )),
                               ),
                             ),
                           ),
@@ -154,22 +159,21 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
                                 sendSMS(message: message, recipients: contacts);
                                 launchUrl(Uri.parse("tel://112"));
                                 Navigator.pop(context, 'OK');
-
                               },
                               child: Container(
                                 width: 100,
                                 height: 50,
                                 decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(25), color: kButtonColor
-                                ),
-                                child: Center(
-                                    child: Text(
-                                      'OKAY',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ),
+                                    borderRadius: BorderRadius.circular(25),
+                                    color: kButtonColor),
+                                child: const Center(
+                                  child: Text(
+                                    'OKAY',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
                             ),
@@ -179,7 +183,7 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
                     ), //new column child
                   ],
                 ),
-              ) ;
+              );
             }));
     showDialog(
         context: ctx,
@@ -209,119 +213,154 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen> {
     final user = _auth.currentUser;
     String userName = "${FirebaseUtility.name} ${FirebaseUtility.surname}";
     return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) =>
-            [
-          MediaQuery.removePadding(
-            context: context,
-            removeTop: true,
-            removeBottom: true,
-            child: SliverAppBar(
-              stretch: true,
-              title: const Circles(),
-              backgroundColor: kBackgroundColor,
-              elevation: 0,
-              toolbarHeight: MediaQuery.of(context).size.height / 6,
-              centerTitle: false,
-              titleSpacing: 0,
-              automaticallyImplyLeading: false,
-            ),
-          ),
-        ],
-        body: MediaQuery.removePadding(
-          context: context,
-          removeTop: true,
-          child: ListView(
-            physics: const BouncingScrollPhysics(),
-            children: [
-              Expanded(
-                  child: Padding(
-                padding: const EdgeInsets.all(8.0),
+        backgroundColor: kBackgroundColor,
+        body: Stack(
+          children: [
+            NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) => [
+                MediaQuery.removePadding(
+                  context: context,
+                  removeTop: true,
+                  removeBottom: true,
+                  child: SliverAppBar(
+                    stretch: true,
+                    title: const Circles(),
+                    backgroundColor: kBackgroundColor,
+                    elevation: 0,
+                    toolbarHeight: MediaQuery.of(context).size.height / 6,
+                    centerTitle: false,
+                    titleSpacing: 0,
+                    automaticallyImplyLeading: false,
+                  ),
+                ),
+              ],
+              body: MediaQuery.removePadding(
+                context: context,
+                removeTop: true,
                 child: ListView(
+                  //TODO her şeyi scrollview içine mi alsak? welcome falan da kaysa, sliver appbar diye bişey var ondan yapılabilir
                   physics: const BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  children: [
-                    Center(
-                        child: Text(
-                      welcomeString,
-                      style: kSignUpInfoStyle.copyWith(
-                          fontSize: MediaQuery.of(context).size.height / 20),
-                    )),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 0,
-                          horizontal: MediaQuery.of(context).size.height / 15),
-                      child: Center(
-                          child: Text(
-                        userName,
-                        style: kSignUpInfoStyle.copyWith(
-                          fontSize: 40,
-                        ),
-                        Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(
-                                    bottom: MediaQuery.of(context).size.height /
-                                        40),
-                                child: RegisterButton(
-                                  title: 'EMERGENCY CALL',
-                                  minWidth:
-                                      MediaQuery.of(context).size.height / 2,
-                                  height:
-                                      MediaQuery.of(context).size.height / 12,
-                                  pressedFunct: () =>{
-                                    _startTimer(),
-                                    alertD(context),
-                                  }
-
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListView(
+                          physics: const BouncingScrollPhysics(),
+                          shrinkWrap: true,
+                          children: <Widget>[
+                            Center(
+                                child: Text(
+                              welcomeString,
+                              style: kSignUpInfoStyle.copyWith(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height / 20),
+                            )),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 0,
+                                  horizontal:
+                                      MediaQuery.of(context).size.height / 15),
+                              child: Center(
+                                  child: Text(
+                                userName,
+                                style: kSignUpInfoStyle.copyWith(
+                                  fontSize: 40,
                                 ),
-                                SizedBox(height: 10.0),
-                                ContactCard(
-                                  page: CallInfo(),
-                                  imageUrl: 'images/dad_girl.png',
-                                  contactName: 'DAD <3',
-                                  durationText: '2min',
-                                  utilitiesText:
-                                      'A two minute relaxing audio with your dad while walking',
-                                ),
-                                SizedBox(height: 10.0),
-                                ContactCard(
-                                  page: CallInfo(),
-                                  imageUrl: 'images/bestie.png',
-                                  contactName: 'BESTIE <3',
-                                  durationText: '2min',
-                                  utilitiesText:
-                                      'A two minute relaxing audio with your best friend while walking',
-                                ),
-                                SizedBox(height: 10.0),
-                                ContactCard(
-                                  page: CallInfo(),
-                                  imageUrl: 'images/lovers.png',
-                                  contactName: 'LOVER <3',
-                                  durationText: '2min',
-                                  utilitiesText:
-                                      'A two minute relaxing audio with your lover while walking',
-                                ),
-                                SizedBox(
-                                    height:
-                                        MediaQuery.of(context).size.height / 8),
-                              ],
+                                textAlign: TextAlign.center,
+                              )),
                             ),
-                          ),
-                        ],
+                            SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height / 40),
+                            Center(
+                                child: Text(startText, style: kSignUpInScreen)),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal:
+                                        MediaQuery.of(context).size.height /
+                                            40),
+                                child: ListView(
+                                  physics: const BouncingScrollPhysics(),
+                                  scrollDirection: Axis.vertical,
+                                  shrinkWrap: true,
+                                  children: [
+                                    ContactCard(
+                                      page: CallInfo(),
+                                      imageUrl: 'images/mom_daughter.png',
+                                      contactName: 'MOM',
+                                      durationText: '2min',
+                                      utilitiesText:
+                                          'A two minute relaxing audio with your mom while walking',
+                                    ),
+                                    const SizedBox(height: 10.0),
+                                    ContactCard(
+                                      page: CallInfo(),
+                                      imageUrl: 'images/dad_girl.png',
+                                      contactName: 'DAD',
+                                      durationText: '2min',
+                                      utilitiesText:
+                                          'A two minute relaxing audio with your dad while walking',
+                                    ),
+                                    const SizedBox(height: 10.0),
+                                    ContactCard(
+                                      page: CallInfo(),
+                                      imageUrl: 'images/bestie.png',
+                                      contactName: 'BESTIE',
+                                      durationText: '2min',
+                                      utilitiesText:
+                                          'A two minute relaxing audio with your best friend while walking',
+                                    ),
+                                    const SizedBox(height: 10.0),
+                                    ContactCard(
+                                      page: CallInfo(),
+                                      imageUrl: 'images/lovers.png',
+                                      contactName: 'LOVER',
+                                      durationText: '2min',
+                                      utilitiesText:
+                                          'A two minute relaxing audio with your lover while walking',
+                                    ),
+                                    SizedBox(
+                                        height:
+                                            MediaQuery.of(context).size.height /
+                                                8),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
-              )),
-            ],
-          ),
-        ),
-      ),
-    );
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).size.height / 40),
+                      child: RegisterButton(
+                          title: 'EMERGENCY CALL',
+                          minWidth: MediaQuery.of(context).size.height / 2,
+                          height: MediaQuery.of(context).size.height / 12,
+                          pressedFunct: () => {
+                                _startTimer(),
+                                alertD(context),
+                              }),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
 /*
