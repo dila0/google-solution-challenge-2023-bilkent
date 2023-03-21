a47de9c89fde1f58daa17cbb06ad971464eb1c60
Ali Tunahan Işık <tunahan0735@gmail.com>
2023-03-21 15:04:18 +0300

    requests merged

--- lib/screens/contact_options_screen.dart
+++ lib/screens/contact_options_screen.dart
@@ -44,24 +44,7 @@ Future<Position> _determinePosition() async {
     return Future.error('Location services are disabled.');
   }
 
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
-    }
-  }
 
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately.
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-  }
 
   // When we reach here, permissions are granted and we can
   // continue accessing the position of the device.
@@ -438,7 +421,26 @@ class _ContactOptionsScreenState extends State<ContactOptionsScreen>
 }
 
 void getPermission()async{
-  await [ Permission.sms ].request();
+  await [ Permission.sms, Permission.locationWhenInUse ].request();
+
+  /*LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }*/
 }
 //}
 /*
